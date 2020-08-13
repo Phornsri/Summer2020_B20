fafa8f994d160cc2993c0011de1ac36be415374f
Phornsri <ponsri_Molae074@yahoo.com>
2020-08-12 21:59:14 -0400

    day40_CustomClass

---
+++ src/day40_CustomClassPractice/Offer.java
@@ -0,0 +1,43 @@
+package day40_CustomClassPractice;
+
+/*
+Offer
+		Attribute:
+			salary, Location, hasPTO, isFullTime, WFH, jobTitle, hasBenefit
+		Actions:
+			setOfferInfo
+			getOfferInfo
+		MUST use this keyword
+ */
+
+
+public class Offer {
+
+    double salary;
+    String state;
+    boolean hasPTO;
+    boolean isFullTime;
+    boolean isWFH;
+    String jobTitle;
+    boolean hasBenefit;
+
+
+    public void setOfferInfo(double salary, String state, boolean hasPTO, boolean isFullTime, boolean isWFH, String jobTitle, boolean hasBenefit){
+        salary  = salary;
+        state = state;
+        hasPTO = hasPTO;
+        isFullTime = isFullTime;
+        isWFH = isWFH;
+        jobTitle = jobTitle;
+        hasBenefit = hasBenefit;
+    }
+
+
+    public void getOfferInfo(){// display the offer info
+        System.out.println("==============================================");
+        System.out.println("Salary: "+salary);
+        System.out.println("JobTitle: "+jobTitle);
+        System.out.println("Location: "+state);
+        System.out.println("==============================================");
+    }
+}
