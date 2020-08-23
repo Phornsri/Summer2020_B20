6ebbf2880b93ff5037542006d9cfc9b784c3ae6a
Phornsri <ponsri_Molae074@yahoo.com>
2020-08-23 09:21:31 -0400

    add day44_Constructor.java

---
+++ src/day43_Static/staticBlock.java
@@ -0,0 +1,30 @@
+package day43_Static;
+/*
+CybertekStudent:
+
+            studentName
+            gender
+            age
+            ID
+        static  SchoolName = "Cybertek School";
+        static  dreamJob = "SDET";
+        static hasRepl.it = true;
+        static tuition
+Book:
+        name
+        author
+        price
+    static  readable
+    static  hasTitle
+
+ */
+
+public class staticBlock {
+
+    public static void main(String[] args) {
+        System.out.println("Main method");
+    }
+    static {
+        System.out.println("Static block");
+    }
+}
